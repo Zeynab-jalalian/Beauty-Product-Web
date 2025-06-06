9efb5a5356d85179ed818eb9c56933f70c8a218b
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-06 11:29:56 +0330

    new products

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let selector='#new-p-heading li';
-$(selector).on('click',function(){
+/* ul filters */
+let selector = '#new-p-heading li';
+$(selector).on('click', function () {
     $(selector).removeClass('active')
     $(this).addClass('active')
+})
+
+$(document).ready(function () {
+    $('#new-p-heading li').click(function () {
+        const value = $(this).attr('data-filter');
+        if (value == 'All') {
+            $('.new-product-box').show('0');
+        } else {
+            $('.new-product-box').not('.' + value).hide('0');
+            $('.new-product-box').filter('.' + value).show('0');
+        }
+    })
 })
